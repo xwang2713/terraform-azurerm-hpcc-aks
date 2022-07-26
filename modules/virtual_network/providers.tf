dc97af5629d89efb2235f7b0d6cb6447a25d45b2
xwang2713 <xiaoming.wang@lexisnexis.com>
2022-07-26 16:18:11 -0400

    test sp 26

--- modules/virtual_network/providers.tf
+++ modules/virtual_network/providers.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   tenant_id       = "bc877e61-f6cf-4461-accd-0565fa4ca357"
   subscription_id = "ec0ba952-4ae9-4f69-b61c-4b96ff470038"
   client_id       = "025881d1-4538-4bee-8194-b1d89351603e"
-  client_secret   = "867c47a0-b204-4237-801a-0b905bc68643"
-  
+  client_secret   = "Dqf8Q~L8pwiJ~AN8XLxYVadDCsRD8OWgY71rAduV"
+
   features {}
 }
