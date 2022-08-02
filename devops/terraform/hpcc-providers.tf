a859b737e63a1b0d5e17499874c6ce5de95e6211
xwang2713 <xiaoming.wang@lexisnexis.com>
2022-08-02 14:31:03 -0400

    test ci 222

--- devops/terraform/hpcc-providers.tf
+++ devops/terraform/hpcc-providers.tf
@@ -38,15 +38,20 @@ provider "kubectl" {
 provider "helm" {
   kubernetes {
     host                   = module.kubernetes.kube_config.host
-    client_certificate     = base64decode(module.kubernetes.kube_config.client_certificate)
-    client_key             = base64decode(module.kubernetes.kube_config.client_key)
+    #client_certificate     = base64decode(module.kubernetes.kube_config.client_certificate)
+    #client_key             = base64decode(module.kubernetes.kube_config.client_key)
     cluster_ca_certificate = base64decode(module.kubernetes.kube_config.cluster_ca_certificate)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "kubelogin"
+      args        = ["get-token", "--login", "spn", "--server-id", var.azure.server_id, "--environment", "AzurePublicCloud", "--tenant-id", var.azure.tenant_id]
+      env         = local.k8s_exec_auth_env
+    }
+
   }
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "kubelogin"
-    args        = ["get-token", "--login", "spn", "--server-id", var.azure.server_id, "--environment", "AzurePublicCloud", "--tenant-id", var.azure.tenant_id]
-    env         = local.k8s_exec_auth_env
+  experiments {
+    manifest = true
   }
 }
