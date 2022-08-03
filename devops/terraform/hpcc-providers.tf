755b3436145897b55eff1147dff6680f18309dc1
xwang2713 <xiaoming.wang@lexisnexis.com>
2022-08-03 08:22:29 -0400

    test ci 228

--- devops/terraform/hpcc-providers.tf
+++ devops/terraform/hpcc-providers.tf
@@ -38,8 +38,8 @@ provider "kubectl" {
 provider "helm" {
   kubernetes {
     host                   = module.kubernetes.kube_config.host
-    client_certificate     = base64decode(module.kubernetes.kube_config.client_certificate)
-    client_key             = base64decode(module.kubernetes.kube_config.client_key)
+    #client_certificate     = base64decode(module.kubernetes.kube_config.client_certificate)
+    #client_key             = base64decode(module.kubernetes.kube_config.client_key)
     cluster_ca_certificate = base64decode(module.kubernetes.kube_config.cluster_ca_certificate)
 
     exec {
