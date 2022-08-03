234bd17494f48f67b907f3c3ab82c80d326c8acd
xwang2713 <xiaoming.wang@lexisnexis.com>
2022-08-03 11:30:29 -0400

    test ci 230

--- devops/terraform/vnet-versions.tf
+++ devops/terraform/vnet-versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<=2.99.0"
+      #version = "<=2.99.0"
+      version = ">=3.16.0"
     }
     random = {
       source  = "hashicorp/random"
