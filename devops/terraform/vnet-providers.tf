23290217c96d9bc16083eaa2d5dcc50778aa2195
xwang2713 <xiaoming.wang@lexisnexis.com>
2022-08-07 20:44:30 -0400

    test ci 294

--- devops/terraform/vnet-providers.tf
+++ devops/terraform/vnet-providers.tf
@@ -1,10 +1,10 @@
 provider "random" {
 }
 provider "azurerm" {
-  tenant_id       = var.azure.tenant_id
-  subscription_id = var.azure.subscription_id
-  client_id       = var.azure.client_id
-  client_secret   = var.azure.client_secret
+  #tenant_id       = var.azure.tenant_id
+  #subscription_id = var.azure.subscription_id
+  #client_id       = var.azure.client_id
+  #client_secret   = var.azure.client_secret
 
   features {}
 }
