8f45b49ceabaa620cb8a9ed4394d48fb6c13e401
xwang2713 <xiaoming.wang@lexisnexis.com>
2022-08-07 22:32:21 -0400

    test ci 300

--- devops/terraform/hpcc-providers.tf
+++ devops/terraform/hpcc-providers.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  tenant_id       = var.azure.tenant_id
-  subscription_id = var.azure.subscription_id
-  client_id       = var.azure.client_id
-  client_secret   = var.azure.client_secret
+  #tenant_id       = var.azure.tenant_id
+  #subscription_id = var.azure.subscription_id
+  #client_id       = var.azure.client_id
+  #client_secret   = var.azure.client_secret
 
   features {}
 }
@@ -13,12 +13,12 @@ provider "kubernetes" {
   client_key             = base64decode(module.kubernetes.kube_config.client_key)
   cluster_ca_certificate = base64decode(module.kubernetes.kube_config.cluster_ca_certificate)
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "kubelogin"
-    args        = ["get-token", "--login", "spn", "--server-id", var.azure.server_id, "--environment", "AzurePublicCloud", "--tenant-id", var.azure.tenant_id]
-    env         = local.k8s_exec_auth_env
-  }
+ # exec {
+ #   api_version = "client.authentication.k8s.io/v1beta1"
+ #   command     = "kubelogin"
+ #   args        = ["get-token", "--login", "spn", "--server-id", var.azure.server_id, "--environment", "AzurePublicCloud", "--tenant-id", var.azure.tenant_id]
+ #   env         = local.k8s_exec_auth_env
+ #}
 }
 
 provider "kubectl" {
@@ -27,27 +27,27 @@ provider "kubectl" {
   load_config_file       = false
   apply_retry_count      = 6
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "kubelogin"
-    args        = ["get-token", "--login", "spn", "--server-id", var.azure.server_id, "--environment", "AzurePublicCloud", "--tenant-id", var.azure.tenant_id]
-    env         = local.k8s_exec_auth_env
-  }
+  #exec {
+  #  api_version = "client.authentication.k8s.io/v1beta1"
+  #  command     = "kubelogin"
+  #  args        = ["get-token", "--login", "spn", "--server-id", var.azure.server_id, "--environment", "AzurePublicCloud", "--tenant-id", var.azure.tenant_id]
+  #  env         = local.k8s_exec_auth_env
+  #}
 }
 
 provider "helm" {
   kubernetes {
     host                   = module.kubernetes.kube_config.host
-    #client_certificate     = base64decode(module.kubernetes.kube_config.client_certificate)
-    #client_key             = base64decode(module.kubernetes.kube_config.client_key)
+    client_certificate     = base64decode(module.kubernetes.kube_config.client_certificate)
+    client_key             = base64decode(module.kubernetes.kube_config.client_key)
     cluster_ca_certificate = base64decode(module.kubernetes.kube_config.cluster_ca_certificate)
 
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "kubelogin"
-      args        = ["get-token", "--login", "spn", "--server-id", var.azure.server_id, "--environment", "AzurePublicCloud", "--tenant-id", var.azure.tenant_id]
-      env         = local.k8s_exec_auth_env
-    }
+    #exec {
+    #  api_version = "client.authentication.k8s.io/v1beta1"
+    #  command     = "kubelogin"
+    #  args        = ["get-token", "--login", "spn", "--server-id", var.azure.server_id, "--environment", "AzurePublicCloud", "--tenant-id", var.azure.tenant_id]
+    #  env         = local.k8s_exec_auth_env
+    #}
 
   }
 
--- main.tf
+++ main.tf
@@ -53,10 +53,10 @@ module "kubernetes" {
   resource_group_name = module.resource_group.name
   identity_type       = "UserAssigned" # Allowed values: UserAssigned or SystemAssigned
   rbac = {
-    #enabled        = false
-    #ad_integration = false
-    enabled        = true
-    ad_integration = true
+    enabled        = false
+    ad_integration = false
+    #enabled        = true
+    #ad_integration = true
   }
 
   network_plugin         = "azure"
@@ -85,12 +85,12 @@ module "kubernetes" {
  #   client_id    = var.azure.client_id
  # }
 
-  rbac_admin_object_ids = {
-      "godji_object_id" = "253519bd-e6c7-4a2e-8ef9-ddb843048f2b"
-      "ming_object_id" = "074407c2-9d5c-4bf9-9b18-35a8b7ae0226"
-      # SP Object ID
-      #"ming_object_id" = "fc0bd818-d698-4089-a6b1-f41cf5d1f4c5"
-  }
+ # rbac_admin_object_ids = {
+ #     "godji_object_id" = "253519bd-e6c7-4a2e-8ef9-ddb843048f2b"
+ #     "ming_object_id" = "074407c2-9d5c-4bf9-9b18-35a8b7ae0226"
+ #     # SP Object ID
+ #     #"ming_object_id" = "fc0bd818-d698-4089-a6b1-f41cf5d1f4c5"
+ # }
 }
 
 resource "kubernetes_secret" "sa_secret" {
